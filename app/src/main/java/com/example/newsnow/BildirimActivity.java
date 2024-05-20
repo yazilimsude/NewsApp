cb9e2c02a776754d7a7ca2f4af108f0ddf837cc5
RanaBetulKaya <rana20032016@icloud.com>
2024-05-20 23:05:54 +0300

    toolbar has been added

--- app/src/main/java/com/example/newsnow/BildirimActivity.java
+++ app/src/main/java/com/example/newsnow/BildirimActivity.java
@@ -1,26 +1,38 @@
 package com.example.newsnow;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class BildirimActivity extends Activity {
+
+public class BildirimActivity extends AppCompatActivity {
     Button kaydet;
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bildirim);
 
-        kaydet = findViewById(R.id.kaydet);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
+        kaydet = findViewById(R.id.kaydet);
         kaydet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(BildirimActivity.this, MainActivity.class);
-                startActivity(intent);
+                
             }
         });
     }
